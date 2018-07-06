ea25c1e27a8fa1ca97320a4603c6de9eaae11246
Laurence N. Béland <laurence.n.beland@gmail.com>
2018-07-06 11:10:22 -0400

    calendar (beginning)

--- script/calendar.js
+++ script/calendar.js
@@ -1,2 +1,18 @@
 "use strict";
 
+let selected_activity = new Activity();
+let sessions_date = [];
+let date;
+let add;
+
+if (selected_activity.timed_sessions.length > 0){
+for (let d of selected_activity.timed_sessions){
+
+    date = d.date;
+    add = sessions_date.push(date);
+
+}
+}
+
+console.log(date);
+console.log(add);
